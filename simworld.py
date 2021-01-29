f43f73b76a5babbf62620bd348efaf26d8fab25a
Emanuele96 <manu969@live.it>
2021-01-29 20:33:35 +0100

    finished graphics

--- simworld.py
+++ simworld.py
@@ -1,26 +1,36 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+from PIL import Image
+import io
 import variables
 class Node:
-    def __init__(self, coordinates):
+    def __init__(self, coordinates, is_empty):
         self.coordinates = coordinates
         self.neighbours = list(())
-
+        self.is_empty = is_empty
+    
+    def empty_the_node(self):
+        self.is_empty = True
 class Board:
-    def __init__(self, form, size):
+    def __init__(self, form, size, empty_nodes):
         self.form = form
         self.size = size
+        self.empty_nodes = empty_nodes
         self.pawns = {}
+        self.populate_board()
+        self.graph = self.generate_graph()
     
     def find_valid_neighbours(self,node):
         #find all possible neighbours using defined direction rules. Save thoose neighbours in the neighboard-list of the node as a tuple (direction, node)  
-        print ("node: " + str(node.coordinates))
+        if variables.debug:
+            print ("node: " + str(node.coordinates))
         if self.form == "diamond":
             possible_neighbors = ((0,-1),(-1,0),(-1,1),(0,1),(1,0),(1,-1))
             for possible_neighbor in possible_neighbors:
                 tmp_coordinate = (node.coordinates[0] + possible_neighbor[0], node.coordinates[1] + possible_neighbor[1])
-                print("tmp_coordinates: " + str(tmp_coordinate))
+                if variables.debug:
+                    print("tmp_coordinates: " + str(tmp_coordinate))
                 if tmp_coordinate != node.coordinates and  tmp_coordinate[0] >=0 and tmp_coordinate[0] < self.size and tmp_coordinate[1] >= 0 and tmp_coordinate[1] < self.size:
                     if self.pawns[tmp_coordinate] not in node.neighbours:
                         node.neighbours.append((possible_neighbor,self.pawns[tmp_coordinate]))          
@@ -35,7 +45,10 @@ class Board:
     def populate_board(self):
         for i in range(self.size):
             for j in range(i+1 if self.form == "triangle" else self.size):
-                node = Node((i,j))   
+                is_empty = False
+                if (i,j) in self.empty_nodes:
+                    is_empty = True
+                node = Node((i,j), is_empty)   
                 self.pawns[(i,j)] = node
         for coordinate in self.pawns:
             self.find_valid_neighbours(self.pawns[coordinate])
@@ -46,7 +59,8 @@ class Board:
         all_nodes = list(())
         for node in self.pawns.keys():
             all_nodes.append(node)
-        print(all_nodes)
+        if variables.debug:
+            print(all_nodes)
         adj_matrix = np.full((len(all_nodes), len(all_nodes)), 0, dtype=int)
         #Then iterate through every node (row) and for each neighbour, find the corrispondent index(column) and fill that box with 1.
         for i in range(len(all_nodes)):
@@ -56,19 +70,70 @@ class Board:
                 adj_matrix[i][j] = 1
         return adj_matrix
 
+    def generate_graph(self):
+        adj_matrix = self.to_numpy_array()
+        G = nx.Graph(adj_matrix)
+        #Add node attributes for "coordinate" and "is_empty", taken from board "pawns" dictionary
+        all_nodes = list(())
+        for node in self.pawns.keys():
+            all_nodes.append(node)
+        for i in range(adj_matrix.shape[0]):
+            G.add_node(i, is_empty = self.pawns[all_nodes[i]].is_empty, diamond_plan = all_nodes[i][0] + all_nodes[i][1], triangle_plan =  all_nodes[i][0] )
+        if variables.debug:
+            print(adj_matrix)
+            print(G.nodes.data())
+        return G
+
+    def show_board(self):
+        #Plot the graph nodes and edges  with regards of plans (1 different plan per row), then do the necessary flipping and rotation for matching the board to the assigment
+        if self.form == "diamond":
+            rotation = 180
+            plan_key = "diamond_plan"
+        elif self.form == "triangle":
+            rotation = 180
+            plan_key = "triangle_plan"
+        position = nx.multipartite_layout(self.graph, subset_key=plan_key, align="horizontal", center=[0,5])
+        #Decide the color of each node, based on if it is empty or not
+        nodes_color = []
+        nodes = list(self.graph.nodes.data("is_empty"))
+        for node in nodes:
+            if node[1] == True:                
+                nodes_color.append("#ffffff")
+            else:
+                nodes_color.append("#000000")
+        
+        options = {'node_size': 400,'width': 1, 'pos' : position, 'with_labels':False, 'font_weight':'bold', 'node_color': nodes_color, 'linewidths':5}
+        nx.draw(self.graph, **options )
+        #outline each node and specify linewidt to show an empty node. ax.collection is the path collection of matplotlib.
+        # documentation :  https://matplotlib.org/3.3.3/api/collections_api.html
+        ax = plt.gca()
+        ax.collections[0].set_edgecolor("#000000") 
+        ax.collections[0].set_linewidth(2)
+        fig = plt.gcf()
+        img = self.matplotlib_to_pil(fig)
+        img = img.rotate(rotation)
+        if self.form == "diamond":
+            img = img.transpose(Image.FLIP_LEFT_RIGHT)
+        plt.clf()
+        img.show()
+        
+
+    def matplotlib_to_pil(self, fig):
+        #Convert a Matplotlib object to an PIL Image   
+        bufffer = io.BytesIO()
+        fig.savefig(bufffer)
+        bufffer.seek(0)
+        return Image.open(bufffer)
+
+empty_nodes = ((1,1), (2,2))
+board = Board("diamond",6, empty_nodes)
+board.show_board()
 
 
-
-board = Board("triangle", 6)
-board.populate_board()
-print(board.pawns)
-for key in board.pawns:
-    for neighbour in board.pawns[key].neighbours:
-        print(str(key) + " : " + str(neighbour[1].coordinates))
-        print(neighbour)
-print(board.pawns[(1,0)].neighbours)
-a = board.to_numpy_array()
-print(a)
-D = nx.Graph(a)
-nx.draw(D, with_labels=True, font_weight='bold')
-plt.show()
+if variables.debug:
+    print(board.pawns)
+    for key in board.pawns:
+        for neighbour in board.pawns[key].neighbours:
+            print(str(key) + " : " + str(neighbour[1].coordinates))
+            print(neighbour)
+    print(board.pawns[(1,0)].neighbours)
--- variables.py
+++ variables.py
@@ -1,4 +1,4 @@
 diamond_neighbors_rule = ((0,-1),(-1,0),(-1,1),(0,1),(1,0),(1,-1))
 triangle_neighbors_rule = ((-1,-1),(-1,0),(0,1),(1,1),(1,0),(0,-1))
 
-debug = True
+debug = False
